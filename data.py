a5c99abd735608d975a4035c944b32eb5f7981ec
Nikita Ivkin <ivkinnikita@gmail.com>
2018-10-20 17:24:51 -0400

    made data.py simple to manipulate streams

--- data.py
+++ data.py
@@ -1,54 +1,56 @@
 import numpy as np
-import random
-import logging
-from math import sqrt
 import argparse, sys
 
 class Data:
     @staticmethod
-    def load(path):
-        data = np.load(path)
-        return data
+    def load(path, binary=1):
+        return np.load(path) if binary else np.array(open(path).read().splitlines())
 
     @staticmethod
-    def onTheFly(n, order=''):
-        orders = ['sorted', 'random']
-        assert (order in orders)
-        if order == 'sorted': 
-            for item in range(n):
-                yield item
-        if order == 'random':  
-            items = list(range(n))
-            random.shuffle(items)
-            for item in items:
-                yield item
+    def syn2csv(streamType="random", streamLen=4): 
+        assert(streamType in ['sorted', 'random'])
+        stream = np.arange(10**streamLen) 
+        if streamType == 'random':  
+            np.random.shuffle(stream)
+        np.savetxt(str(streamLen) + streamType + ".csv",stream, fmt="%u")
 
     @staticmethod
-    def gen2file(path, n, order, binary):
-        data = np.zeros(n)
-        for item_i, item in enumerate(Data.onTheFly(n, order)):
-            if item_i >= n:
-                break;
-            data[item_i] = item
-        if binary == "binary": 
-            np.save(path, data)
-        else:
-            np.savetxt(path, data, fmt="%u")
+    def csv2npy(path,t="str"): 
+        assert(t in ['str', 'int'])
+        lines = open(path).read().splitlines()
+        if t == 'int': 
+            lines = map(int, lines) 
+        stream = np.array(lines)  
+        np.save(path[:-4], stream)
+
+    @staticmethod
+    def printout(path):
+        assert(path[-3:] in ['csv', 'npy'])
+        binary = 0 if path[-3:] == "csv" else 1
+        data = Data.load(path, binary)  
+        for item in data:
+            print item
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-l', type=int, default=6, 
+    parser.add_argument('-a', type=str,
+                        help='''action: 'generate' to generate synthetic dataset, 
+                        'convert' to save csv as binary and 'read' to print stream from file ''')
+    parser.add_argument('-l', type=int, default=4, 
                         help='''length of the stream (set x to get 10^x)''')
     parser.add_argument('-o', type=str, default="random", 
-                         help='''stream order: random/sorted''')
-    parser.add_argument('-f', type=str, default="length_order", 
-                         help='''path to the output file''')
-    parser.add_argument('-b', type=str, default="text", 
-                         help='''output file binary/text''') 
-    args = parser.parse_args()
-    length = args.l ; order = args.o; filename = args.f; binary = args.b;
-    if filename == "length_order":
-        filename = str(length) + "_" + order    
-    Data.gen2file(filename, 10**length, order, binary)
+                        help='''stream type: random/sorted''')
+    parser.add_argument('-t', type=str, default="str", 
+                        help='''item type: 'str' or 'int' ''')
+    parser.add_argument('-f', type=str, 
+                        help='''path to file''')
     
-   
+    args = parser.parse_args()
+    if args.a  == 'generate':
+        Data.syn2csv(args.o, args.l)
+    elif args.a  == 'convert' and args.f:
+        Data.csv2npy(args.f, t=args.t)
+    elif args.a  == 'read' and args.f:
+        Data.printout(args.f) 
+    
+  
