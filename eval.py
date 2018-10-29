plot.py: added plotting for exp1(space vs precision); eval.py: added code for running exp2 (different c parameter) kll.py: changed layer sizes allocation (was bug there, i.e. serious loss in memory due to bad choice of k based on space limit)

eval.py
@@ -76,8 +76,24 @@ def exp1(streamLen, streamsPath, repsN, threadsN):
                        (0,0,1,1),(1,0,1,1),(0,1,1,1),(1,1,1,1)]} 
     runManySettings(algo, params, streams, repsN, threadsN)
    
-
+def exp2(streamLen, streamsPath, repsN, threadsN):
+    # loading all streams of length streamLen
+    streams = ["random", "sorted"]
+    types = ["int", "int"]
+    streams = [streamsPath + str(streamLen)+ i + ".csv" for i in streams]
+    streams = zip(streams, types) 
    
+    # header for the output
+    print("dataset|mode|c|maxError|errorStd|meanError") 
+   
+    # running experiments for KLL for all modes and spaces  
+    algo = getattr(kll,"KLL") 
+    for c in np.arange(10,90)/100: 
+        params = {"s": [1024], "c": c,
+                  "mode": [(0,0,0,0),(1,0,0,0)]} 
+        runManySettings(algo, params, streams, repsN, threadsN)
+  
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', type=str, default="exp1", 

plot.py
@@ -20,7 +20,7 @@ def plotExp1(resFilePath):
     for stream in res.keys():
         fig, ax = plt.subplots()
         lines = [] 
-        algos = ['lwyc', 'kll0000', 'kll1000', 'kll1100', 'kll1010', 'kll1001','kll1101']
+        algos = ['lwyc', 'kll0000', 'kll1000', 'kll0100', 'kll0010', 'kll0001']
         for algo in algos:
             data = np.array(res[stream][algo])
             line, = ax.plot(range(len(data[:,0])),data[:,1]/10**6, linewidth=2, label= algo)